4c6d252b2f7aa0d763ac00593c2ab8514abed230
juanferGG <juanfe15777@gmail.com>
2023-06-09 06:38:03 -0500

    task Complete

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('btn')
+const adviceHtml = document.getElementById('advice')
+const adviceH2 = document.getElementById('adviceH2')
+const line = document.getElementById('line')
+
+async function getMethod(){
+    const data = await fetch(`https://api.adviceslip.com/advice/${Math.floor(Math.random() * 90)}`).then((e) => e.json())
+    adviceH2.style.display = 'block'
+    advice.style.display = 'block'
+    adviceH2.textContent = `Advice: #${data.slip.id}`
+    adviceHtml.textContent = `"${data.slip.advice}"`
+    line.style.display = 'block'
+
+    console.log(data)
+}
+
+btn.addEventListener('click', getMethod)
